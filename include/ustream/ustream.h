cab2fdd46fe2236a5b856ae4d61c2af535a1024f
ThomasAUB <aubert.thms@gmail.com>
2022-07-02 18:08:46 +0200

    replace rw function by a structure

--- include/ustream/ustream.h
+++ include/ustream/ustream.h
@@ -9,67 +9,66 @@ struct Channel {
 
     template<typename return_t, typename ... args_t>
     static bool setImmutable(return_t (*inFunc)(args_t...)) {
-        bool ioSuccess = false;
-        rw<false>(ioSuccess, true, inFunc);
-        return ioSuccess;
+        return FuncContainer<false, return_t, args_t...>::write(inFunc);
     }
 
     template<typename return_t, typename ... args_t>
     static void setMutable(return_t (*inFunc)(args_t...)) {
-        bool _;
-        rw<true>(_, true, inFunc);
+        FuncContainer<true, return_t, args_t...>::write(inFunc);
     }
 
     template<bool is_mutable, typename return_t, typename ... args_t>
     static auto get() {
-        bool _;
-        return rw<is_mutable, return_t, args_t...>(_);
+        return FuncContainer<is_mutable, return_t, args_t...>::read();
     }
 
     template<typename return_t, typename ... args_t>
     static void erase() {
-        bool _;
-        rw<true, return_t, args_t...>(_, true);
+        FuncContainer<true, return_t, args_t...>::write();
     }
 
     template<bool is_mutable, typename return_t, typename ... args_t>
     static bool exists() {
-        bool ioSuccess = false;
-        rw<is_mutable, return_t, args_t...>(ioSuccess);
-        return ioSuccess;
+        return FuncContainer<is_mutable, return_t, args_t...>::exists();
     }
 
 private:
 
     template<bool is_mutable, typename return_t, typename ... args_t>
-    static auto rw(bool &ioSuccess, bool inWrite = false, return_t (*inFunc)(args_t...) = nullptr) {
+    struct FuncContainer {
 
         static constexpr auto stub = [](args_t...) { return return_t(); };
 
         using f_t = return_t(*)(args_t...);
 
-        static f_t sFunction = stub;
+        static inline f_t sFunction = stub;
 
-        ioSuccess = true;
+        static bool write(f_t inFunc = nullptr) {
 
-        if (inWrite) {
             if (is_mutable || sFunction == stub) {
                 if (inFunc) {
                     sFunction = inFunc;
                 } else {
                     sFunction = stub;
                 }
+                return true;
             } else {
                 // write attempt on an already set immutable
-                ioSuccess = false;
+                return false;
             }
-        } else { // read
-            // true if sFunction has been set
-            ioSuccess = (sFunction != stub);
+
         }
 
-        return &sFunction;
-    }
+        static auto read() {
+            return &sFunction;
+        }
+
+        static bool exists() {
+            return (sFunction != stub);
+        }
+
+    };
+
 
 };
 
@@ -84,6 +83,14 @@ struct Socket {
     template<auto id>
     void attach() {
         mFunc = Channel<id>::template get<is_mutable, return_t, args_t...>();
+    }
+
+    template<auto id>
+    bool detach() {
+        if(is_mutable) {
+            mFunc = const_cast<function_t>(&stub);
+        }
+        return is_mutable;
     }
 
     template<auto id>
