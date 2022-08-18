7d1e5cfd89c0d4588db1324268a2b754774e86d0
Thomas AUBERT <thomas.aubert@arturia.com>
2022-08-18 14:07:26 +0200

    add socket file

--- include/ustream/ustream.h
+++ include/ustream/ustream.h
@@ -65,32 +65,6 @@ private:
 
 };
 
-template<typename return_t, typename... args_t>
-struct Socket;
-
-template<typename return_t, typename... args_t>
-struct Socket<return_t(args_t...)> {
-
-    inline return_t operator()(args_t ... args) const;
-
-    template<auto id>
-    void attach();
-
-    void detach();
-
-    template<auto id>
-    bool exists();
-
-private:
-
-    static constexpr return_t(*stub)(args_t...) = [](args_t...) { return return_t(); };
-
-    using function_t = return_t(**)(args_t...);
-    function_t mFunc = const_cast<function_t>(&stub);
-
-};
-
-
 
 
 
@@ -160,34 +134,6 @@ template<typename return_t, typename ... args_t>
 bool Channel<id>::exists() {
     using c_t = Caller<return_t, args_t...>;
     return (c_t::sFunction != c_t::stub);
-}
-
-
-
-template<typename return_t, typename... args_t>
-return_t Socket<return_t(args_t...)>::operator()(args_t ... args) const {
-    return (*mFunc)(args...);
-}
-
-template<typename return_t, typename... args_t>
-template<auto id>
-void Socket<return_t(args_t...)>::attach() {
-    mFunc = Channel<id>::template get<return_t, args_t...>();
-}
-
-template<typename return_t, typename... args_t>
-void Socket<return_t(args_t...)>::detach() {
-    mFunc = const_cast<function_t>(&stub);
-}
-
-template<typename return_t, typename... args_t>
-template<auto id>
-bool Socket<return_t(args_t...)>::exists() {
-    return
-        (
-            (mFunc != const_cast<function_t>(&stub)) &&
-            Channel<id>::template exists<return_t, args_t...>()
-        );
 }
 
 
