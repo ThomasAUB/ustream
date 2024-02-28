90cc9fca65276d0138c79a8ffe69798af50d4388
Thomas AUBERT <aubert.thms@gmail.com>
2024-02-28 12:34:12 +0100

    start github action unit test setup

---
+++ test/tests.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+//#include "ustream/.h"
+
+
+TEST_CASE("basic uStream tests") {
+
+    CHECK(true);
+
+}
+
