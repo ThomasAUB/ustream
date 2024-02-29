596967ab2d14288c7925360a084f3fd39e228d18
Thomas AUBERT <aubert.thms@gmail.com>
2024-02-29 17:37:01 +0100

    add header description

--- include/ustream/broadcast.hpp
+++ include/ustream/broadcast.hpp
@@ -1,3 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * MIT License                                                                     *
+ *                                                                                 *
+ * Copyright (c) 2024 Thomas AUBERT                                                *
+ *                                                                                 *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy    *
+ * of this software and associated documentation files (the "Software"), to deal   *
+ * in the Software without restriction, including without limitation the rights    *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       *
+ * copies of the Software, and to permit persons to whom the Software is           *
+ * furnished to do so, subject to the following conditions:                        *
+ *                                                                                 *
+ * The above copyright notice and this permission notice shall be included in all  *
+ * copies or substantial portions of the Software.                                 *
+ *                                                                                 *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   *
+ * SOFTWARE.                                                                       *
+ *                                                                                 *
+ * github : https://github.com/ThomasAUB/uStream                                   *
+ *                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #pragma once
 
 #include "islot.hpp"
@@ -6,14 +33,13 @@
 namespace ustream {
 
     template<auto address, typename ... args_t>
-    bool openSlot(ISlot<args_t...>& s);
+    bool open(ISlot<args_t...>& s);
 
     template<typename ... args_t>
-    void closeSlot(ISlot<args_t...>& s);
+    void close(ISlot<args_t...>& s);
 
     template<auto address, typename ... args_t>
     void broadcast(args_t&&... args);
-
 
 
     namespace detail {
@@ -53,7 +79,7 @@ namespace ustream {
 
     template<typename ... args_t>
     void close(ISlot<args_t...>& s) {
-        s.close();
+        s.disconnect();
     }
 
 }
